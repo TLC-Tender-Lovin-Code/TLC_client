d0d9116fd551b574366d92cc6156f78b36a645dd
Cyn-D <cynthia.l.douglas@gmail.com>
2020-08-25 14:09:29 -0400

    Updated cURL scripts
    Updated Sign In code to include username
    Add note to messages in AutoDismissAlert for next steps.

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -1,3 +1,7 @@
+// We will want to add the username to the Sign In success message. I think we should do this by
+// using ${credentials.username} in the signIn Success message using tic marks. The question is,
+// where do we import that data from?
+
 export default {
   signUpSuccess: 'Succesfully registered! You\'ve been signed in as well.',
   signUpFailure: 'Registration failed. Email may be taken, or passwords don\'t match.',
