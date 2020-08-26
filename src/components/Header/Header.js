b47a9d3d2aeb0dd69de5d66996df9f52ec6cb812
Cyn-D <cynthia.l.douglas@gmail.com>
2020-08-26 11:02:22 -0400

    Update for updating post resource
    updated:
    - UpdatePost.js
    - updated App/App.js
    - added shared/footer.js and shared/layout.js
    - updated AutoDismissAlert/messages.js
    - added devpost curl script for update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,7 @@ const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <Nav.Link href="#update-post">Update Post</Nav.Link>
   </Fragment>
 )
 
