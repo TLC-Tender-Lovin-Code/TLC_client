339c3ac346cf1070d2db2e5487175e3ac148dbbd
Cyn-D <cynthia.l.douglas@gmail.com>
2020-08-27 15:35:39 -0400

    Don't ignore my cases

---
+++ src/components/devPost/UpdatePost.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import { Redirect, Link, withRouter } from 'react-router-dom'
+import Layout from '../shared/Layout'
+// import messages from '../AutoDismissAlert/messages'
+import { updatePost, showPost } from '../../api/devpost'
+
+const UpdatePost = ({ user, match }) => {
+  const [devpost, setDevpost] = useState({ title: '', subject: '', content: '' })
+  const [updated, setUpdated] = useState(false)
+
+  useEffect(() => {
+    showPost(user, devpost, match.params.id)
+      .then(res => setDevpost(res.data.devpost))
+      .catch(console.error)
+  }, [])
+
+  const handleChange = event => {
+    event.persist()
+    setDevpost(prevDevpost => {
+      const updatedField = { [event.target.name]: event.target.value }
+      const editedPost = Object.assign({}, prevDevpost, updatedField)
+      return editedPost
+    })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    // const { user } = this.props
+    console.log(user)
+    console.log(devpost)
+    updatePost(user, devpost, match.params.id)
+      .then(() => setUpdated({ updated: true }))
+      // .then(() => msgAlert({
+      //   heading: 'Update Post Success',
+      //   message: messages.updatePostSuccess,
+      //   variant: 'success'
+      // }))
+      // .msgAlert({
+      //   heading: 'Update Post Failed. ',
+      //   message: messages.updatePostFailure,
+      //   variant: 'danger'
+      // })
+      .catch(console.error)
+  }
+
+  if (updated) {
+    return <Redirect to='/devposts' />
+  }
+
+  return (
+    <Layout>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          placeholder="Enter a title"
+          value={devpost.title}
+          name="title"
+          onChange={handleChange}
+          type="text"
+        /><br />
+
+        <label>Subject</label>
+        <input
+          placeholder="Enter a subject"
+          value={devpost.subject}
+          name="subject"
+          onChange={handleChange}
+          type="text"
+        /><br />
+
+        <label>Content</label>
+        <textarea
+          placeholder="Enter text here ..."
+          value={devpost.content}
+          name="content"
+          onChange={handleChange}
+          type="text"
+        /><br />
+
+        <button className='btn btn-warning' type="submit">Update</button>
+        <Link to='/'>
+          <button className='btn btn-primary'>Cancel</button>
+        </Link>
+      </form>
+    </Layout>
+  )
+}
+
+export default withRouter(UpdatePost)
