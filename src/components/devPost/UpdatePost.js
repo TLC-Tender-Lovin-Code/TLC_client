106d4f5d9bb4d8ebebd63755f541c91d7d4982a9
TheLog <lgscustomww@gmail.com>
2020-08-27 14:51:57 -0500

    added uppercase and told git not to ignore.

--- src/components/devPost/UpdatePost.js
+++ src/components/devPost/UpdatePost.js
@@ -76,6 +76,10 @@ const UpdatePost = ({ msgAlert, user, match }) => {
         <button className='btn btn-warning' type="submit">Update</button>
         <Link to='/'>
           <button className='btn btn-primary'>Cancel</button>
+          <button type="submit">Update</button>
+        </Link>
+        <Link to='/'>
+          <button>Cancel</button>
         </Link>
       </form>
     </Layout>
